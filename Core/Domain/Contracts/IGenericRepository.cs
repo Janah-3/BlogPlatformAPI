2469545078132bc61cddcb7cec61346e04fe98dc
jana <jana.ayoub.004@gmail.com>
2025-07-04 23:55:26 +0300

    generic repositoriy interface and implementation

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entites;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity ,TKey > where TEntity : BaseEntity<TKey>
+    {
+        Task<TEntity> GetByIdAsync(TKey id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+       
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Contracts;
+using Domain.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly AppDbContext _dbContext;
+        public GenericRepository(AppDbContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+      
+
+        public async Task AddAsync(TEntity entity) => _dbContext.Set<TEntity>().AddAsync(entity);
+
+        public void Delete(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
+
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbContext.Set<TEntity>().ToListAsync();
+
+
+        public async Task<TEntity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+
+        public void Update(TEntity entity) => _dbContext.Set<TEntity>().Update(entity);
+
+    }
+}
